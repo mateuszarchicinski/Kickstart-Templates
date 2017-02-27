da5f4cf608c94c4d285ecaa3e3d27174dfec4075
mateuszarchicinski <mateusz.archicinski@gmail.com>
2017-02-27 02:08:33 +0100

    Improv

--- Angular 1.x.x/gulpfile.js
+++ Angular 1.x.x/gulpfile.js
@@ -2,8 +2,8 @@
 
 
 // NODE MODULES
-var gulp = require('gulp'),
-    $ = require('gulp-load-plugins')({
+var gulp = require('gulp'), // https://gulp.readme.io/docs/getting-started
+    $ = require('gulp-load-plugins')({ // https://github.com/jackfranklin/gulp-load-plugins#options
         lazy: true
     }),
     jshintStylish = require('jshint-stylish'),
@@ -40,17 +40,16 @@ function getOption(option){
 // Compiling SASS to CSS, adding right prefixes to CSS methods - depending on configuration, creating source map, finally injects CSS styles into browser
 gulp.task('sass:css', function () {
 
-    $.util.log($.util.colors.green('SASS TO CSS TASK RUNNING...')); // https://www.npmjs.com/package/gulp-util
+    $.util.log($.util.colors.green('SASS TO CSS TASK RUNNING...')); // https://github.com/gulpjs/gulp-util#usage
 
     return gulp.src(PROJECT_CONFIG.DIRECTORY.WORK_DIR + '/sass/main.s+(a|c)ss')
-        .pipe($.plumber()) // https://www.npmjs.com/package/gulp-plumber
-        .pipe($.sourcemaps.init()) // https://www.npmjs.com/package/gulp-sourcemaps
+        .pipe($.plumber()) // https://github.com/floatdrop/gulp-plumber#monkey-gulp-plumber
+        .pipe($.sourcemaps.init()) // https://github.com/floridoo/gulp-sourcemaps#init-options
         .pipe($.sass.sync({ // https://github.com/sass/node-sass#options
             outputStyle: 'nested' // compact - compressed - expanded - nested
         }))
         .pipe($.autoprefixer({ // https://github.com/postcss/autoprefixer#options
             browsers: ['last 5 version'],
-            cascade: false,
             stats: ['> 1%']
         }))
         .pipe($.sourcemaps.write('./maps'))
@@ -60,7 +59,7 @@ gulp.task('sass:css', function () {
 });
 
 
-// Validate Syntactically Awesome Style Sheets (SASS) Code, atm custom configuration for more info / rules check ---> https://github.com/sasstools/sass-lint#configuring
+// Validates Syntactically Awesome Style Sheets (SASS) Code, atm custom configuration for more info / rules check ---> https://github.com/sasstools/sass-lint#configuring
 gulp.task('sass:lint', function () {
 
     $.util.log($.util.colors.cyan('SASS LINT TASK RUNNING...'));
@@ -86,7 +85,7 @@ gulp.task('sass:lint', function () {
 });
 
 
-// Validate JavaScript Code, atm default configuration for more info check ---> http://jshint.com/docs/
+// Validates JavaScript (JS) Code, atm default configuration for more info check ---> http://jshint.com/docs/
 gulp.task('js:hint', function () {
 
     $.util.log($.util.colors.cyan('JS HINT TASK RUNNING...'));
@@ -99,7 +98,7 @@ gulp.task('js:hint', function () {
 });
 
 
-// Runs unit tests in Karma environment which is configurable via karma.conf.js
+// Runs unit tests in Karma environment which is configurable via karma.conf.js for more info check ---> https://karma-runner.github.io/1.0/index.html
 gulp.task('js:test', function () {
 
     $.util.log($.util.colors.cyan('JS TEST TASK RUNNING...'));
@@ -120,12 +119,12 @@ gulp.task('jade:pug', function() {
             base: PROJECT_CONFIG.DIRECTORY.WORK_DIR
         })
         .pipe($.plumber())
-        .pipe($.rename({ // https://www.npmjs.com/package/gulp-rename
+        .pipe($.rename({ // https://github.com/hparra/gulp-rename#usage
             extname: '.pug'
         }))
         .pipe(gulp.dest(PROJECT_CONFIG.DIRECTORY.WORK_DIR + '/'))
         .on('end', function(){
-            del(PROJECT_CONFIG.DIRECTORY.WORK_DIR + '/template/**/*.jade') // https://www.npmjs.com/package/del
+            del(PROJECT_CONFIG.DIRECTORY.WORK_DIR + '/template/**/*.jade') // https://github.com/sindresorhus/del#usage
         });
     
 });
@@ -147,7 +146,7 @@ gulp.task('pug', function () {
             base: './' + PROJECT_CONFIG.DIRECTORY.WORK_DIR + '/template'
         })
         .pipe($.plumber())
-        .pipe($.data(function(){ // https://www.npmjs.com/package/gulp-data
+        .pipe($.data(function(){ // https://github.com/colynb/gulp-data#gulp-data
             return {
                 appName: PROJECT_CONFIG.APP_NAME,
                 data: JSON.parse(fs.readFileSync('./' + PROJECT_CONFIG.DATA_FILE, 'utf8')).lang[lang]
@@ -157,7 +156,7 @@ gulp.task('pug', function () {
             pretty: true,
             compileDebug: true
         }))
-        .pipe(wiredep({ // https://pugjs.org/api/getting-started.html
+        .pipe(wiredep({ // https://github.com/taptapship/wiredep#wiredep--
             exclude: ['animatewithsass', 'angular-mocks', 'bower_components/angular-material/angular-material.css'],
             ignorePath: '../'
         }))
@@ -166,14 +165,14 @@ gulp.task('pug', function () {
 });
 
 
-// Validate Pug / Jade Code, atm custom configuration for more info ---> https://github.com/pugjs/pug-lint
+// Validates Pug / Jade Code, atm custom configuration for more info check ---> https://github.com/pugjs/pug-lint#pug-lint
 gulp.task('pug:lint', function () {
 
     $.util.log($.util.colors.cyan('PUG LINT TASK RUNNING...'));
     
     return gulp.src(PROJECT_CONFIG.DIRECTORY.WORK_DIR + '/template/**/*.pug')
         .pipe($.plumber())
-        .pipe($.pugLint({
+        .pipe($.pugLint({ // https://github.com/pugjs/pug-lint/blob/master/docs/rules.md
             disallowDuplicateAttributes: true,
             disallowMultipleLineBreaks: true,
             disallowSpacesInsideAttributeBrackets: true,
@@ -183,7 +182,7 @@ gulp.task('pug:lint', function () {
             requireSpecificAttributes: [{img: ['alt']}],
             validateAttributeQuoteMarks: '\"',
             validateDivTags: true
-        })); // https://github.com/pugjs/pug-lint/blob/master/docs/rules.md
+        }));
 
 });
 
@@ -197,22 +196,22 @@ gulp.task('html', function () {
             base: PROJECT_CONFIG.DIRECTORY.WORK_DIR
         })
         .pipe($.plumber())
-        .pipe($.useref()) // https://www.npmjs.com/package/gulp-useref#api
+        .pipe($.useref()) // https://github.com/jonkemp/gulp-useref#usage
         .pipe($.if('*.css', $.cleanCss())) // https://github.com/jakubpawlowicz/clean-css#--------
-        .pipe($.if('*.js', $.uglify())) // {preserveComments: 'license'} ~ https://www.npmjs.com/package/gulp-uglify#options
+        .pipe($.if('*.js', $.uglify())) // {preserveComments: 'license'} ~ https://github.com/terinjokes/gulp-uglify#options
         .pipe(gulp.dest(PROJECT_CONFIG.DIRECTORY.DIST_DIR + '/'));
 
 });
 
 
-// 
+// Validates HyperText Markup Language (HTML) code, atm custom configuration for more info / rules check ---> https://github.com/bezoerb/gulp-htmlhint#api
 gulp.task('html:hint', function () {
 
     $.util.log($.util.colors.cyan('HTML HINT TASK RUNNING...'));
 
     return gulp.src([PROJECT_CONFIG.DIRECTORY.WORK_DIR + '/*.html', PROJECT_CONFIG.DIRECTORY.WORK_DIR + '/views/*.html'])
         .pipe($.plumber())
-        .pipe($.htmlhint({
+        .pipe($.htmlhint({ // https://github.com/yaniswang/HTMLHint/wiki/Rules
             'tagname-lowercase': true,
             'attr-lowercase': true,
             'attr-value-double-quotes': true,
@@ -225,12 +224,12 @@ gulp.task('html:hint', function () {
             'src-not-empty': true,
             'title-require': true
         }))
-        .pipe($.htmlhint.reporter(htmlhintStylish));
+        .pipe($.htmlhint.reporter(htmlhintStylish)); // https://github.com/bezoerb/gulp-htmlhint#reporters
 
 });
 
 
-// 
+// Minify HTML code, atm custom configuration for more info check ---> https://github.com/jonschlinkert/gulp-htmlmin#gulp-htmlmin---
 gulp.task('html:minify', function () {
 
     $.util.log($.util.colors.green('HTML MINIFY TASK RUNNING...'));
@@ -239,40 +238,51 @@ gulp.task('html:minify', function () {
             base: PROJECT_CONFIG.DIRECTORY.DIST_DIR
         })
         .pipe($.plumber())
-        .pipe($.htmlmin({
-            minifyCSS: true
+        .pipe($.htmlmin({ // https://github.com/kangax/html-minifier#options-quick-reference
+            collapseWhitespace: true,
+            minifyCSS: true,
+            minifyJS: true,
+            removeComments: true
         }))
         .pipe(gulp.dest(PROJECT_CONFIG.DIRECTORY.DIST_DIR + '/'));
 
 });
 
 
-// 
+// Runs locally server which allows to sync file changes with browser preview and to browse websites using connected devices, more info about module BROWSER SYNC ---> https://www.browsersync.io/docs/gulp
 gulp.task('server', function () {
 
     $.util.log($.util.colors.red('SERVER TASK RUNNING...'));
 
-    return browserSync.init({
+    return browserSync.init({ // https://www.browsersync.io/docs/options
         server: PROJECT_CONFIG.DIRECTORY.WORK_DIR + '/'
     });
 
 });
 
 
-// 
+// Watching file changes then runs appropriate tasks
 gulp.task('watch', function () {
 
     $.util.log($.util.colors.blue('WATCH TASK RUNNING...'));
-
+    
     gulp.watch(PROJECT_CONFIG.DIRECTORY.WORK_DIR + '/sass/**/*.s+(a|c)ss', ['sass:lint', 'sass:css']);
-    gulp.watch(PROJECT_CONFIG.DIRECTORY.WORK_DIR + '/js/**/*.js', ['js:hint', browserSync.reload]);
+    gulp.watch(PROJECT_CONFIG.DIRECTORY.WORK_DIR + '/js/**/*.js', ['js:watch']);
     gulp.watch([PROJECT_CONFIG.DATA_FILE, PROJECT_CONFIG.DIRECTORY.WORK_DIR + '/template/**/*.pug', 'bower.json'], ['pug:lint', 'pug']);
-    gulp.watch([PROJECT_CONFIG.DIRECTORY.WORK_DIR + '/*.html', PROJECT_CONFIG.DIRECTORY.WORK_DIR + '/views/*.html'], ['html:hint', browserSync.reload]);
+    gulp.watch([PROJECT_CONFIG.DIRECTORY.WORK_DIR + '/*.html', PROJECT_CONFIG.DIRECTORY.WORK_DIR + '/views/*.html'], ['html:watch']);
 
 });
 
 
-// 
+// Created to fix problem with browserSync.reload, more info ---> https://github.com/BrowserSync/browser-sync/issues/717#issuecomment-119713757
+gulp.task('js:watch', ['js:hint'], browserSync.reload);
+
+
+// Created to fix problem with browserSync.reload, more info ---> https://github.com/BrowserSync/browser-sync/issues/717#issuecomment-119713757
+gulp.task('html:watch', ['html:hint'], browserSync.reload);
+
+
+// Removes production directory, more info about module DEL ---> https://github.com/sindresorhus/del#del--
 gulp.task('clean', function () {
 
     $.util.log($.util.colors.gray('CLEAN TASK RUNNING...'));
@@ -282,7 +292,7 @@ gulp.task('clean', function () {
 });
 
 
-// 
+// Copies a files from the working directory to the production directory
 gulp.task('copy', function () {
 
     $.util.log($.util.colors.grey('COPY TASK RUNNING...'));
@@ -296,13 +306,13 @@ gulp.task('copy', function () {
 });
 
 
-// 
+// Optimizes images by using standard modules avaible via NPM or by TinyPNG API ---> https://tinypng.com/developers/reference/nodejs
 gulp.task('images', function () {
 
     $.util.log($.util.colors.magenta('IMAGES TASK RUNNING...'));    
     
-    var condition = getOption('--option').value === 'advanced',
-        optimizationModule = condition ? require('gulp-tinify') : require('gulp-imagemin'),
+    var condition = getOption('--option').value === 'advanced', // https://github.com/joshbroton/gulp-tinify#gulp-tinify
+        optimizationModule = condition ? require('gulp-tinify') : require('gulp-imagemin'), // https://github.com/sindresorhus/gulp-imagemin#api
         args = PROJECT_CONFIG.API_KEYS.TINIFY;
     
     if(!condition){
@@ -324,7 +334,7 @@ gulp.task('images', function () {
 });
 
 
-// 
+// Creates so-called spritesheet which consists files .png thet merged multiple images and .scss that styles  ---> https://github.com/Ensighten/spritesmith#spritesmith--
 gulp.task('images:sprite', function () {
 
     $.util.log($.util.colors.magenta('IMAGES SPRITE TASK RUNNING...'));
@@ -337,14 +347,14 @@ gulp.task('images:sprite', function () {
         $.util.log($.util.colors.magenta('Default options passed to images:sprite task. To change that, add command arguments to this task ---> gulp [TASK NAME = images:sprite] --name [_FILE_NAME].'));
     }
     
-    return gulp.src(PROJECT_CONFIG.DIRECTORY.WORK_DIR + '/img/sprites_sources/**/*.{jpg,png,gif}')
+    gulp.src(PROJECT_CONFIG.DIRECTORY.WORK_DIR + '/img/sprites_sources/**/*.{jpg,png,gif}')
         .pipe($.plumber())
-        .pipe($.spritesmith({
+        .pipe($.spritesmith({ // https://github.com/twolfson/gulp.spritesmith#documentation
             imgName: spriteImgName + '.png',
             cssName: spriteCssName + '.scss',
             imgPath: '../img/' + spriteImgName + '.png'
         }))
-        .pipe($.if('*.png', gulp.dest(PROJECT_CONFIG.DIRECTORY.WORK_DIR + '/img/'), gulp.dest(PROJECT_CONFIG.DIRECTORY.WORK_DIR + '/sass/components/sprites/')));
+        .pipe($.if('*.png', gulp.dest(PROJECT_CONFIG.DIRECTORY.WORK_DIR + '/img/'), gulp.dest(PROJECT_CONFIG.DIRECTORY.WORK_DIR + '/sass/components/sprites/'))); // https://github.com/robrich/gulp-if#gulp-if-api
 
 });
 
--- Angular 1.x.x/src/js/app.js
+++ Angular 1.x.x/src/js/app.js
@@ -1,1 +1,3 @@
+/* jshint ignore:start */
 var app = angular.module('app', ['ui.router', 'ngMaterial']);
+/* jshint ignore:end */
